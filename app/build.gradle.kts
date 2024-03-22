b35ae1de9233dac50850a85a71210e8a70fe87c9
Alessandro Sisto <alessandro.sisto@dualb.it>
2024-03-22 16:16:00 +0100

    add viewBinding

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -69,6 +69,8 @@ android {
     buildFeatures {
         buildConfig = true
         compose = true
+        viewBinding = true
+        dataBinding = true
     }
     composeOptions {
         kotlinCompilerExtensionVersion = Versions.compose_compiler
