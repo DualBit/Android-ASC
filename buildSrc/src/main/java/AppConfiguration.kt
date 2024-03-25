c477d958b1f8046358686865e2a11b1bf0b4b974
Alessandro Sisto <alessandro.sisto@dualb.it>
2024-03-25 11:42:06 +0100

    version bump

--- buildSrc/src/main/java/AppConfiguration.kt
+++ buildSrc/src/main/java/AppConfiguration.kt
@@ -1,6 +1,6 @@
 object AppConfiguration {
     const val application_id = "com.android.amity.sample"
     const val application_name = "AndroidAmity"
-    const val version_code = 1
-    const val version_name = "1.0"
+    const val version_code = 1001
+    const val version_name = "1.1"
 }
